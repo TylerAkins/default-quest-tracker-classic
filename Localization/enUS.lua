637c6f9364497498e981cdcc013650e5c734cbad
Tyler Akins <tyler.akins@gocrisp.com>
2026-08-13 19:16:32 -0500

    Filter unavailable quests and hide AQ War Effort offers

--- Localization/enUS.lua
+++ Localization/enUS.lua
@@ -3,7 +3,7 @@ local ADDON_NAME = ...
 _G[ADDON_NAME] = _G[ADDON_NAME] or {}
 local DQTC = _G[ADDON_NAME]
 DQTC.name = ADDON_NAME
-DQTC.version = "1.0.2"
+DQTC.version = "1.0.3"
 
 local L = {}
 DQTC.L = L
